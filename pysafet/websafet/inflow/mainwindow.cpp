d006a8be463b861c97f6aea0c4826a5eefe76d6f
Victor Bravo <victor@llosa>
2015-03-19 21:36:32 -0430

    agregando valor por defecto

--- pysafet/websafet/inflow/mainwindow.cpp
+++ pysafet/websafet/inflow/mainwindow.cpp
@@ -1394,6 +1394,8 @@ QString MainWindow::generateFormHead(const QString& o) {
                 */
     }
 
+    result += " $('#id').val(6).change();\n";
+
     if (keymodifyfields.count() > 0 ) {
     	result += "});\n\n";
 	
